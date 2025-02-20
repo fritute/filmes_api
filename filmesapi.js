25d5b18e2a8fa93dbc97d690ed529eba70832a75
GUSTAVO ZUMBA DE AMORIM <24124573@cfp127aluno.local>
2025-02-20 12:02:26 -0300

    style pronto

--- filmesapi.js
+++ filmesapi.js
@@ -2,22 +2,27 @@
 
 async function pesquisarFotos(filme) {
   const url = `https://imdb.iamidiotareyoutoo.com/search?q=${filme}`
-  const response = await fetch(url);
-  const data = await response.json();
+  const response = await fetch(url)
+  const data = await response.json()
 
   if (response.ok) {
-    return data.description;
+    return data.description
   } else {
     console.error(`Erro ao buscar fotos: ${response.status}`)
     return [];
   }
 }
 
-function criarImagem(link) {
+function criarImagem(link, nome, ano) {
   const galeria = document.getElementById('galeria')
+  const novaDiv = document.createElement('div')
   const novaImg = document.createElement('img')
   novaImg.src = link
-  galeria.appendChild(novaImg)
+  const nomeFilme = document.createElement('p')
+  nomeFilme.textContent = `${nome} (${ano})`
+  novaDiv.appendChild(novaImg)
+  novaDiv.appendChild(nomeFilme)
+  galeria.appendChild(novaDiv)
 }
 
 async function preencherFotosdeFilmes() {
@@ -29,10 +34,10 @@ async function preencherFotosdeFilmes() {
   const fotos = await pesquisarFotos(filme)
   if (fotos && fotos.length > 0) {
     fotos.forEach((foto) => {
-      if (foto['#IMG_POSTER']) {
-        criarImagem(foto['#IMG_POSTER'])
+      if (foto['#IMG_POSTER'] && foto['#TITLE'] && foto['#YEAR']) {
+        criarImagem(foto['#IMG_POSTER'], foto['#TITLE'], foto['#YEAR'])
       }
-    });
+    })
   } else {
     console.log('Nenhuma foto encontrada')
   }
